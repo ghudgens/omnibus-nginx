f84e4fc1a825137ba50bc1dd253041c89eead811
Grant Hudgens <grant.hudgens@gmail.com>
2014-06-05 10:30:22 -0400

    Initial commit of base ruby nginx with ldap.

--- config/projects/nginx.rb
+++ config/projects/nginx.rb
@@ -1,11 +1,32 @@
+#
+# Copyright:: Copyright (c) 2014 Grant Hudgens.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 name 'nginx'
-maintainer 'CHANGE ME'
-homepage 'CHANGEME.com'
+maintainer 'nginx'
+homepage 'nginx.org'
 
 install_path    '/opt/nginx'
-build_version   Omnibus::BuildVersion.semver
-build_iteration 1
+build_version   '1.6.0'
+build_iteration 0
+
+# Specify dependency versions to embed
+override :'ruby', version: "2.0.0-p451"
+override :'passenger-gem', version: "4.0.42"
+override :'nginx', version: "1.6.0"
 
 # creates required build directories
 dependency 'preparation'

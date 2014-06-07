0d28289a36a9c7e8d7c69f16db402ff285cd16fc
Grant Hudgens <grant.hudgens@gmail.com>
2014-06-06 20:48:23 -0400

    Trying to fix zlib path issue...

--- config/software/libpng.rb
+++ config/software/libpng.rb
@@ -27,14 +27,17 @@ source :url => "http://downloads.sourceforge.net/libpng/libpng-#{version}.tar.gz
 
 relative_path "libpng-#{version}"
 
-configure_env = {
-  "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-  "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-  "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
-}
+env = { "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+        "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+        "LD_RUN_PATH" => "#{install_dir}/embedded/lib"}
 
 build do
-  command "./configure --prefix=#{install_dir}/embedded --with-zlib-prefix=#{install_dir}/embedded", :env => configure_env
-  command "make -j #{max_build_jobs}", :env => {"LD_RUN_PATH" => "#{install_dir}/embedded/lib"}
-  command "make install"
+  configure_command = ["./configure",
+                       "--prefix=#{install_dir}/embedded",
+                       "--with-zlib-prefix=#{install_dir}/embedded"]
+
+  command configure_command.join(" "), :env => env
+
+  command "make -j #{max_build_jobs}", :env => env
+  command "make install", :env => env
 end
